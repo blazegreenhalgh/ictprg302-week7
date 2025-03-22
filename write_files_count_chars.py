3a40be14324007a16cae82ff2ac2a515f9513782
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-03-22 15:47:26 +0800

    Created functionality for counting upper and lower case

--- write_files_count_chars.py
+++ write_files_count_chars.py
@@ -10,16 +10,24 @@
 
 
 user_file = input("File to be checked (romeojuliet.txt): ").lower()
-user_letter = input("Letter to be counted: ").lower()
+
+user_letter_lower = input("Letter to be counted: ").lower()
+user_letter_upper = user_letter_lower.upper()
+
+# Reviewed: This list is completely unnecessary.
+user_letter = [user_letter_lower, user_letter_upper]
 
 with open(f"sources/{user_file}", "r") as file:
     file_read = file.read()
 
-letter_count = file_read.count(user_letter)
+
+# Reviewed: The pop() could have just been user_letter_lower and user_letter_upper.
+letter_count = file_read.count(user_letter.pop())
+letter_count += file_read.count(user_letter.pop())
 
 with open(f"sources/{user_file}", "a") as file:
-    file.write(f"\nLetter: {user_letter}")
-    file.write(f"\nOccurrences: {letter_count}")
+    file.write(f"\nLetter: {user_letter_upper}{user_letter_lower}")
+    file.write(f"\nTotal Occurrences: {letter_count}")
 
 with open(f"sources/{user_file}", "r") as file:
     file_read = file.read()
